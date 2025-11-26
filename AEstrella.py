c7483b5ca686cf4c1916a66bbb7902041d417b34
Belthoor <belthor@outlook.es>
2025-11-26 19:34:23 +0100

    comentarios beltri

--- AEstrella.py
+++ AEstrella.py
@@ -3,21 +3,26 @@ from math import radians, cos, sin, asin, sqrt
 
 class AEstrella:
     def __init__(self, mapa_objeto):
-        """
-        :param mapa_objeto: Instancia de la clase Mapa ya inicializada.
-        """
+        # Constructor de la clase AEstrella:
+        # Extraemos el grafo completo (nodos y aristas con sus pesos g(n)):
         self.mapa = mapa_objeto.get_grafo()
+        # Extraemos las coordenadas de las (latitud, longitud):
         self.posiciones = mapa_objeto.get_posiciones()
 
     def heuristica_haversine(self, nodo_actual, nodo_destino):
-        """
-        Calcula la distancia Haversine (distancia en esfera) entre dos puntos
-        geográficos. Esta es la función h(n).
-        """
-        # Obtener coordenadas (lat, lon)
+        # Funcion heurística f(n)
+        # Calcula la distancia en linea recta entre dos estaciones
+        # Se utiliza la fomrula de Haversine ya que la tierra al ser redonda las 
+        # distancia no se pueden calcular de manera lineas.
+
+
+        # Intentar obtener las coordenadas de ambos nodos, self.posiciones es como un
+        # diccionario, {"Observatorio": (19.3244, -99.1738)}
         try:
             lat1, lon1 = self.posiciones[nodo_actual]
             lat2, lon2 = self.posiciones[nodo_destino]
+        
+        # Si no se encuentran las coordenadas, retornamos 0
         except KeyError:
             # Si un nodo no tiene coordenadas (por seguridad), retornamos 0
             return 0
@@ -27,19 +32,18 @@ class AEstrella:
         lat2, lon2 = map(radians, (lat2, lon2))
 
         # Fórmula de Haversine
-        dlon = lon2 - lon1
-        dlat = lat2 - lat1
+        dlon = lon2 - lon1 # Diferencia de longitud entre estaciones
+        dlat = lat2 - lat1 # Diferencia de latitud entre estaciones
+        # 'a' es el cuadrado de la mitad de la distancia entre las dos estaciones
         a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-        c = 2 * asin(sqrt(a))
+        c = 2 * asin(sqrt(a)) #distancia angular en radianes
         r = 6371000  # Radio de la Tierra en metros
         
-        distancia_recta = c * r
+        distancia_recta = c * r #distancia = angulo * radio
         return distancia_recta
 
     def encontrar_ruta(self, estacion_origen_id, estacion_destino_id):
-        """
-        Ejecuta A* usando networkx y la heurística personalizada.
-        """
+        # Método que ejecuta el algoritmo A*
         try:
             print(f"Calculando ruta A* de {estacion_origen_id} a {estacion_destino_id}...")
             
